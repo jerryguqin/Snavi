df3934744d80d7655004e715a5eb57229f133e66
jerryguqin <60022274+jerryguqin@users.noreply.github.com>
2023-05-14 18:40:46 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         iziToast.show({
             title: hello,
-            message: '欢迎来到 Snavigation'
+            message: '欢迎来到 五月的天de搜索页'
         });
     }, 800);
 
